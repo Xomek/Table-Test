6a709b354fdbd524bcedab8b39ad0a5582262066
XomekX <z_v_m_x@mail.ru>
2022-06-23 21:33:39 +0700

    #1 created Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Card } from "../../components";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default Home;
