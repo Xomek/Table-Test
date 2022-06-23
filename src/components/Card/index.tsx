1c7ff0d5700054513001d7217c689db54fe2236b
XomekX <z_v_m_x@mail.ru>
2022-06-23 22:54:49 +0700

    #1 implemented card opening

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useState } from "react";
 import { filterStyles } from "../../helpers/filterStyles";
 import { CardInterface } from "../../interfaces/card.interface";
 import styles from "./Card.module.scss";
@@ -10,18 +10,19 @@ interface CardPropsInterface extends HTMLAttributes<HTMLDivElement> {
 
 const Card: FC<CardPropsInterface> = ({ className, card, ...props }) => {
   const CardStyles = filterStyles([className, styles.card]);
+  const [visible, setVisible] = useState<boolean>(false);
   // const { title, subTitle, dateStart, dateEnd } = card;
 
   return (
     <div className={CardStyles} {...props}>
-      <div className={styles.header}>
+      <div className={styles.header} onClick={() => setVisible(!visible)}>
         <div>
           <div className={styles.title}>x5gdz</div>
           <div className={styles.subTitle}>x5gdz</div>
         </div>
         <div className={styles.data}>02.02.2022 - 03.03.2033</div>
       </div>
-      <Table />
+      {visible && <Table />}
     </div>
   );
 };
